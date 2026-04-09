b0b24a8b4f497544f21482e6ebd8013893dd4c6d
danalonadv-oss <danalonadv@gmail.com>
2026-04-09 18:43:39 +0300

    fix: clean analytics setup - remove duplicate loaders

--- public/analytics.js
+++
@@ -1,8 +0,0 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-NPHZ1VS3LY');
-gtag('consent', 'default', {
-  'ad_storage': 'granted',
-  'analytics_storage': 'granted'
-});
